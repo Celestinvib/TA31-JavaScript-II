6a01cd8eeb18763730a22c7f568144c945d9a962
Celestinvib <celestinvib@gmail.com>
2022-09-03 01:36:22 +0200

    Ex13 v1.0

---
+++ ex13/js/script.js
@@ -0,0 +1,10 @@
+
+function showMsg1 () {
+   alert('Se presionó un párrafo del documento.');
+}
+
+function showMsg2 () {
+   alert('Se presionó un párrafo contenido en la segunda tabla.');
+}
+    
+
